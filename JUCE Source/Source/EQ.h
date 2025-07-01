eccfd1194adab884c8a929ce13fcbdd01194be6b
Reuben Betts <30935412+reubenwbetts@users.noreply.github.com>
2025-07-01 09:35:39 +0100

    Update EQ.h

--- JUCE Source/Source/EQ.h
+++ JUCE Source/Source/EQ.h
@@ -26,12 +26,8 @@ class corrective_eq
     
     private:
 
-
-        // biquads would have made a much better choice as they're static but ah well
-
         svf filter_hs {1500.37, 0.707, 24000.0};
         svf filter_hs2 {2000.0, 0.303, 24000.0};
-
 
         svf filter_hmb {2103.0, 0.4, 24000.0};
         svf filter_hmb2 {2103.0, 0.4, 24000.0};
@@ -39,12 +35,9 @@ class corrective_eq
         svf filter_lmb {180.0, 0.4, 24000.0};
         svf filter_lmb2 {180.0, 0.4, 24000.0};
 
-
         svf filter_ls {150.0, 0.707, 24000.0};
         svf filter_ls2 {300.0, 0.303, 24000.0};
-    
-    
-    
+      
         biquad peak_notch_lm;
         biquad peak_notch_hm;
 
